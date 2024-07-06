9181a82cd6484df43abc64929eb5d54b63892a44
Tushar Karn <tricksburp@gmail.com>
2024-07-06 14:50:55 +0530

    Date And Time

---
+++ 01-Fundamentals-Part-1/07_datesinJs.js
@@ -0,0 +1,39 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2003, 10, 22)
+let myNewDate = new Date(2003, 10, 22, 12, 30, 25)
+
+console.log(myNewDate.toLocaleString());
+console.log(myNewDate);
+console.log(myNewDate.toDateString());
+
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+
+})
